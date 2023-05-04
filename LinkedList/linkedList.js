5324605132970f2da9bc1942931b2373fd07a42d
fasilkp <fasilkp314@gmail.com>
2023-05-04 16:32:55 +0530

    linked list append completed

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -24,7 +24,31 @@ class LinkedList {
     }
 
 
-   
+    append(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node
+        } else {
+            let currrent = this.head
+            while (currrent.next) {
+                currrent = currrent.next
+            }
+            currrent.next=node;
+        }
+        this.size++;
+    }
+
+
+    getELements() {
+        if (this.isEmpty()) {
+            console.log("no values found")
+        }
+        let currrent = this.head
+        while (currrent) {
+            console.log(currrent.value);
+            currrent = currrent.next
+        }
+    }
 
 
 }
@@ -32,5 +56,15 @@ class LinkedList {
 
 
 const linkedList = new LinkedList()
+linkedList.prepend(5)
+linkedList.prepend(6)
+linkedList.prepend(7)
 
+linkedList.append(8)
+linkedList.prepend(10)
+linkedList.append(11)
+
+
+
+linkedList.getELements()
 

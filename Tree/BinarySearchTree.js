f06e41581181965a6dce9ba2bc4750ba72a9c245
fasilkp <fasilkp314@gmail.com>
2023-05-20 12:08:21 +0530

    binary search tree inorder traversal completed

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -37,6 +37,30 @@ class BST{
             }
         }
     }
+    search(target){
+        let current= this.root;
+        while(current){
+            if(current.value===target){
+                return true;
+            }
+            else if(target<current.value){
+                current=current.left;
+            }
+            else if(target>current.value){
+                current=current.right;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+    }
+    inOrder(node){
+        if(!node) return;
+        this.inOrder(node.left);
+        console.log(node.value)
+        this.inOrder(node.right)
+    }
 
 }
 
@@ -44,5 +68,15 @@ const bst= new BST()
 bst.insert(10)
 bst.insert(20)
 bst.insert(7)
-bst.insert(8)
+bst.insert(9)
+
+console.log(bst.search(7))
+console.log(bst.search(8))
+console.log(bst.search(10))
+console.log(bst.search(20))
+console.log(bst.search(70))
+console.log(bst.search(80))
 console.log(bst)
+bst.inOrder(bst.root)
+
+
